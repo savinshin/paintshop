2237340d30fde03b28f44b4052bd061539ac0b04
savinshin <savinbelilya@gmail.com>
2026-07-24 12:44:35 +0200

    Update production API URL

--- PaintShopFrontEnd/src/environments/environment.ts
+++ PaintShopFrontEnd/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'https://paintshop-production.up.railway.app',
+  apiBaseUrl: 'https://paintshop-backend-production.up.railway.app',
   supabaseUrl: 'https://stijacsdwvbldoqcthpi.supabase.co',
   supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0aWphY3Nkd3ZibGRvcWN0aHBpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE3NTc1MDYsImV4cCI6MjA3NzMzMzUwNn0.dGnr4NNyXnutIYYYLgWlVw8vDgewVDkyoFdBksMqsGY',
 } as const;
